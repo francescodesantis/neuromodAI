76037b6553d387ac4374f4595b0a920f8cb22efa
RiccardoCasciotti <riccardo.casciotti.1998@gmail.com>
2024-04-19 20:43:21 +0200

    try

--- SoftHebb-main/ray_search.py
+++ SoftHebb-main/ray_search.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
     params = parser.parse_args()
     #ray.init(include_dashboard=False, local_mode=True)
     #print("Params:    " + params)
-    ray.init(_temp_dir="$HOME")
+    #ray.init(_temp_dir="$HOME")
     config = get_config(params.config)
 
     params.name_model = params.preset if params.model_name is None else params.model_name  # TODO change this for better model storage
--- SoftHebb-main/utils.py
+++ SoftHebb-main/utils.py
@@ -10,7 +10,8 @@ import torch.nn as nn
 from torch.optim.lr_scheduler import StepLR
 
 username = op.expanduser('~').split('/')[-1]
-data_candidate = ('/scratch' if 'hrodriguez' == username else '/home') + f'/{username}/workspace'
+#data_candidate = ('/scratch' if 'hrodriguez' == username else '/home') + f'/{username}/workspace'
+data_candidate = "$HOME"
 DATA = op.realpath(op.expanduser(data_candidate))
 RESULT = op.join(DATA, 'results', 'hebb', 'result')  # everything from multi_layer.py
 SEARCH = op.join(DATA, 'results', 'hebb', 'search')  # everything from ray_search
