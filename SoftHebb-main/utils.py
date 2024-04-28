f8bfe4971c903641d8ea26fa97bf74fbe0057d32
RiccardoCasciotti <riccardo.casciotti.1998@gmail.com>
2024-04-28 17:24:23 +0200

    -

--- SoftHebb-main/utils.py
+++ SoftHebb-main/utils.py
@@ -460,6 +460,8 @@ def load_config_dataset(name=None, validation=True):
         dataset_config['val_sample'] = int(
             np.floor(dataset_config['training_sample'] * dataset_config['validation_split']))
         dataset_config['training_sample'] = dataset_config['training_sample'] - dataset_config['val_sample']
+
+    print(dataset_config)
     return dataset_config
 
 
