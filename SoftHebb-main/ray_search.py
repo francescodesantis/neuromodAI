03e6ff6f0e96a15f1c79582debbdeab2d7af8b8c
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-07-03 11:38:32 +0200

    -

--- SoftHebb-main/ray_search.py
+++ SoftHebb-main/ray_search.py
@@ -192,6 +192,7 @@ def main(params, dataset_sup_config, dataset_unsup_config, blocks, config):
     log = Log(train_config)
     for id, config in train_config.items():
         if config['mode'] == 'unsupervised':
+            print("BLOCKS: ", config['blocks'])
             run_unsup(
                 config['nb_epoch'],
                 config['print_freq'],
