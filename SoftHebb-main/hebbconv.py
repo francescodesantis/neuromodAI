e71da365695f409dcbf64d8f09a546af74d56b99
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-07-09 16:27:59 +0200

    -

--- SoftHebb-main/hebbconv.py
+++ SoftHebb-main/hebbconv.py
@@ -12,7 +12,7 @@ import einops
 from tabulate import tabulate
 
 from activation import Triangle
-x = 1
+xyz = 1
 
 class HebbHardConv2d(nn.Module):
     def __init__(
@@ -830,9 +830,9 @@ class HebbSoftKrotovConv2d(HebbSoftConv2d):
             out_channels, batch_size, height_out, width_out
         ).transpose(0, 1)
 
-        if x == 1: 
+        if xyz == 1: 
             print("WTA shape in SoftKrotov: ", wta.shape)
-            x = 2
+            xyz = 2
         return wta
 
     def radius(self) -> float:
