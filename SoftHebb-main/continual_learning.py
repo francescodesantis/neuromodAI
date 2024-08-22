ad26dbc2b792b981fc9d56551b00333e86bed298
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-08-22 20:50:39 +0200

    cl

--- SoftHebb-main/continual_learning.py
+++ SoftHebb-main/continual_learning.py
@@ -198,15 +198,15 @@ if __name__ == '__main__':
 
     resume = params.resume
 
-    params.continual_learning = False
+    params.continual_learning = "False"
     params.resume = None
     procedure(params, blocks,dataset_sup_config_1, dataset_unsup_config_1, False)
 
-    params.continual_learning = True
+    params.continual_learning = "True"
     params.resume = resume
     procedure(params, blocks,dataset_sup_config_2, dataset_unsup_config_2, evaluate=False)
 
-    params.continual_learning = False
+    params.continual_learning = "False"
     procedure(params, blocks,dataset_sup_config_1, dataset_unsup_config_1, True)
  
     # resume all problem
