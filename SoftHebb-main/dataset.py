d61c3c67c7482d2c6583221a24ffdef211fbc321
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-09-05 17:54:33 +0200

    cl

--- SoftHebb-main/dataset.py
+++ SoftHebb-main/dataset.py
@@ -272,7 +272,8 @@ def make_data_loaders(dataset_config, batch_size, device, dataset_path=DATASET):
         
     if dataset_config["cl"] == True:
         print("INSIDE CL ###############################")
-        old_dataset_size = dataset_config["old_dataset_size"]
+        #old_dataset_size = dataset_config["old_dataset_size"]
+        old_dataset_size = 264
         origin_dataset = dataset_train_class(
         dataset_path,
         split=split,
