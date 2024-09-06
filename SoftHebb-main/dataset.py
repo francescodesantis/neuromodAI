fc993394dd349972d6fd5fc0c1ac1cc0c02f5d65
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-09-06 12:50:02 +0200

    cl

--- SoftHebb-main/dataset.py
+++ SoftHebb-main/dataset.py
@@ -319,8 +319,9 @@ def make_data_loaders(dataset_config, batch_size, device, dataset_path=DATASET):
     split=split,
     train=True,
     download=not dataset_config['name'] in ['ImageNet'],  # TODO: make this depend on whether dataset exists or not
-    transform=transforms.Compose([#transforms.ToPILImage(),
-                                                    transform,
+    transform=transforms.Compose([    transform,              
+                                    transforms.ToPILImage(),
+                                                    
                                                     transforms.Resize(old_dataset_size, interpolation=transforms.InterpolationMode.NEAREST),  # image size int or tuple
                                                     # Add more transforms here
                                                    
