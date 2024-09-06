4a8b64a35ef8f4794b9bcf9fdccf8b765ebd87f6
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-09-06 11:25:45 +0200

    cl

--- SoftHebb-main/dataset.py
+++ SoftHebb-main/dataset.py
@@ -282,7 +282,7 @@ def make_data_loaders(dataset_config, batch_size, device, dataset_path=DATASET):
         train=True,
         download=not dataset_config['name'] in ['ImageNet'],  # TODO: make this depend on whether dataset exists or not
         transform=transforms.Compose([transform,
-                                                    transforms.Resize(old_dataset_size, interpolation=torch.InterpolationMode.NEAREST),  # image size int or tuple
+                                                    transforms.Resize(old_dataset_size, interpolation=transforms.InterpolationMode.NEAREST),  # image size int or tuple
                                                     # Add more transforms here
                                                     #transforms.ToTensor(),  # convert to tensor at the end
                                                     ]), 
