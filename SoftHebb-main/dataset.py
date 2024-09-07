4dae4b31b0c1e8ffa9426314cc55f7f942c62724
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-09-07 16:45:47 +0200

    cl

--- SoftHebb-main/dataset.py
+++ SoftHebb-main/dataset.py
@@ -338,7 +338,7 @@ def make_data_loaders(dataset_config, batch_size, device, dataset_path=DATASET):
         transform=transforms.Compose([transform, #transforms.ToPILImage(),
                                                     transforms.Resize((old_dataset_size, old_dataset_size), interpolation=transforms.InterpolationMode.NEAREST),  # image size int or tuple
                                                     # Add more transforms here
-                                                    transforms.ToTensor()
+                                                    #transforms.ToTensor()
                                                     # convert to tensor at the end
                                                       
                                                     ]), 
