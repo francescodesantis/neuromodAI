f0ceb243000cbfa18cd90ea707ec537f29ebc46c
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-09-18 20:48:08 +0200

    bug fix

--- SoftHebb-main/ray_search_cl.py
+++ SoftHebb-main/ray_search_cl.py
@@ -304,10 +304,9 @@ def main(blocks, name_model, resume, save, dataset_sup_config, dataset_unsup_con
     #save_logs(log, name_model)
     print("Name Model: ", name_model)
     
-    datas = load_data(name_model, train_config)
-    for d in datas: 
-
-        print("Datas: ", d)
+    #datas = load_data(name_model, train_config)
+    # for d in datas: 
+    #     print("Datas: ", d)
     #print("RESULTS: ", results)
 
 
@@ -440,8 +439,38 @@ if __name__ == '__main__':
             params.continual_learning = False
             params.resume = None
             evaluate = False
-            ray_search(params, dataset_sup_config, dataset_unsup_config, evaluate, results)
+            #ray_search(params, dataset_sup_config, dataset_unsup_config, evaluate, results)
+            config = get_config(params.config, params)
+            reporter = CLIReporter(max_progress_rows=12)
+            for metric in metric_names:
+                reporter.add_metric_column(metric)
 
+            algo_search = BasicVariantGenerator()
+
+            scheduler = ASHAScheduler(
+            grace_period=20, reduction_factor=3, max_t=100_000)
+            params.continual_learning = False
+            blocks = load_presets(params.preset)
+
+            
+            trial_exp = partial(
+                    procedure, params, blocks, dataset_sup_config, dataset_unsup_config, evaluate, results
+            )
+
+            analysis = tune.run(
+                trial_exp,
+                resources_per_trial={
+                        "cpu": 4,
+                        "gpu": torch.cuda.device_count()
+                    },
+                metric=params.metric,
+                mode='min' if params.metric.endswith('loss') else 'max',
+                search_alg=algo_search,
+                config=config,
+                progress_reporter=reporter,
+                num_samples=params.num_samples,
+                local_dir=SEARCH,
+                name=params.model_name)
             # TASK 2
             #selected_classes = random_n_classes(all_classes, n_classes)
             # selected_classes = [2, 1]
