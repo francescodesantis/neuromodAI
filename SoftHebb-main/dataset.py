f9d0796736751f5e7edc39342f22c6d108cbcb99
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2024-09-22 00:14:21 +0200

    try3

--- SoftHebb-main/dataset.py
+++ SoftHebb-main/dataset.py
@@ -738,49 +738,49 @@ class FastCIFAR100(CIFAR100):
 
         norm = transforms.Normalize(mean, std)
 
-        self.data = torch.tensor(self.data, dtype=torch.float, device=device).div_(255)
+    #     self.data = torch.tensor(self.data, dtype=torch.float, device=device).div_(255)
 
-        if self.train:
-            if not isinstance(train_class, str):
-                index_class = np.isin(self.targets, train_class)
-                self.data = self.data[index_class]
-                self.targets = np.array(self.targets)[index_class]
-                self.len = self.data.shape[0]
-                print(self.len)
+    #     if self.train:
+    #         if not isinstance(train_class, str):
+    #             index_class = np.isin(self.targets, train_class)
+    #             self.data = self.data[index_class]
+    #             self.targets = np.array(self.targets)[index_class]
+    #             self.len = self.data.shape[0]
+    #             print(self.len)
 
-        if zca:
-            self.data = (self.data - mean) / std
-            self.zca = whitening_zca(self.data)
-            zca_whitening = transforms.LinearTransformation(self.zca, torch.zeros(self.zca.size(1)))
-        self.data = torch.tensor(self.data, dtype=torch.float)
+    #     if zca:
+    #         self.data = (self.data - mean) / std
+    #         self.zca = whitening_zca(self.data)
+    #         zca_whitening = transforms.LinearTransformation(self.zca, torch.zeros(self.zca.size(1)))
+    #     self.data = torch.tensor(self.data, dtype=torch.float)
 
-        self.data = torch.movedim(self.data, -1, 1)  # -> set dim to: (batch, channels, height, width)
-        # self.data = norm(self.data)
-        if zca:
-            self.data = zca_whitening(self.data)
-            print(self.data.mean(), self.data.std())
+    #     self.data = torch.movedim(self.data, -1, 1)  # -> set dim to: (batch, channels, height, width)
+    #     # self.data = norm(self.data)
+    #     if zca:
+    #         self.data = zca_whitening(self.data)
+    #         print(self.data.mean(), self.data.std())
 
-        # self.data = self.data.to(device)  # Rescale to [0, 1]
+    #     # self.data = self.data.to(device)  # Rescale to [0, 1]
 
-        # self.data = self.data.div_(CIFAR10_STD) #(NOT) Normalize to 0 centered with 1 std
+    #     # self.data = self.data.div_(CIFAR10_STD) #(NOT) Normalize to 0 centered with 1 std
 
-        self.targets = torch.tensor(self.targets, device=device)
+    #     self.targets = torch.tensor(self.targets, device=device)
 
-    def __getitem__(self, index: int):
-        """
-        Parameters
-        ----------
-        index : int
-            Index of the element to be returned
+    # def __getitem__(self, index: int):
+    #     """
+    #     Parameters
+    #     ----------
+    #     index : int
+    #         Index of the element to be returned
 
-        Returns
-        -------
-            tuple: (image, target) where target is the index of the target class
-        """
-        img = self.data[index]
-        target = self.targets[index]
+    #     Returns
+    #     -------
+    #         tuple: (image, target) where target is the index of the target class
+    #     """
+    #     img = self.data[index]
+    #     target = self.targets[index]
 
-        return img, target
+    #     return img, target
 
 
 class AugFastCIFAR100(FastCIFAR100):
