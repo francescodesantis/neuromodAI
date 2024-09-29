02edd0b9500ebd6964e5a95122e00bb8171b15f4
riccardo.casciotti.1998@gmail.com <rcasciot@login03.g100.cineca.it>
2024-09-29 23:27:40 +0200

    bug fix

--- batches/classes_CL/cl.sh
+++ batches/classes_CL/cl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 pwd=$(pwd)
+rm /g100_work/EIRI_E_POLIMI/rcasciot/neuromodAI/SoftHebb-main/TASKS_CL.json
+rm -rf -d /g100_work/EIRI_E_POLIMI/rcasciot/neuromodAI/SoftHebb-main/Images/TASKS_CL
 cd continual_learning
 for file in $pwd/continual_learning/*
 do
--- batches/fd.sh
+++ batches/fd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 pwd=$(pwd)
+rm -rf -d /g100_work/EIRI_E_POLIMI/rcasciot/neuromodAI/SoftHebb-main/Images/MULTD_CL
+rm /g100_work/EIRI_E_POLIMI/rcasciot/neuromodAI/SoftHebb-main/MULTD_CL.json
 cd full_datasets_CL
 for file in $pwd/full_datasets_CL/*
 do
