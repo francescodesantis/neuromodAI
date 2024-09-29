29380b1d46c080107a1b3265787e79a1a5aee556
riccardo.casciotti.1998@gmail.com <rcasciot@login03.g100.cineca.it>
2024-09-29 23:46:28 +0200

    bug fix

---
+++ batches/full_datasets_CL/not_working/C100_IMG.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks-per-node=1         # 32 tasks per node
+#SBATCH --cpus-per-task=4
+#SBATCH --time=04:00:00               # time limits: 1/2 hour
+#SBATCH --error=C100_IMG/job.err            # standard error file
+#SBATCH --output=C100_IMG/job.out           # standard output file
+#SBATCH --account=EIRI_E_POLIMI     # account name
+module load profile/deeplrn
+module av cineca-ai
+cd $WORK/rcasciot/neuromodAI/SoftHebb-main
+rm -rf -d Training/results/hebb/result/network/CIFAR100_ImageNette_CL
+cp -r Training/results/hebb/result/network/CIFAR100_Best Training/results/hebb/result/network/CIFAR100_ImageNette_CL
+rm -rf -d Training/results/hebb/result/network/CIFAR100_ImageNette_CL/models
+mkdir Training/results/hebb/result/network/CIFAR100_ImageNette_CL/models
+cp Training/results/hebb/result/network/CIFAR100_ImageNette_CL/checkpoint.pth.tar Training/results/hebb/result/network/CIFAR100_ImageNette_CL/models/checkpoint.pth.tar
+conda run -n softhebb python continual_learning.py --preset 4SoftHebbCnnCIFAR --resume all --model-name 'CIFAR100_ImageNette_CL' --dataset-unsup-1 CIFAR100_1 --dataset-sup-1 CIFAR100_50 --dataset-unsup-2 ImageNette_1 --dataset-sup-2 ImageNette_50 --continual_learning True --skip-1 True --evaluate True 
+
---
+++ batches/full_datasets_CL/not_working/STL10_C100.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks-per-node=1         # 32 tasks per node
+#SBATCH --cpus-per-task=4
+#SBATCH --time=04:00:00               # time limits: 1/2 hour
+#SBATCH --error=STL10_C100/job.err            # standard error file
+#SBATCH --output=STL10_C100/job.out           # standard output file
+#SBATCH --account=EIRI_E_POLIMI     # account name
+module load profile/deeplrn
+module av cineca-ai
+cd $WORK/rcasciot/neuromodAI/SoftHebb-main
+rm -rf -d Training/results/hebb/result/network/STL10_CIFAR100_CL
+cp -r Training/results/hebb/result/network/STL10_Best Training/results/hebb/result/network/STL10_CIFAR100_CL
+rm -rf -d Training/results/hebb/result/network/STL10_CIFAR100_CL/models
+mkdir Training/results/hebb/result/network/STL10_CIFAR100_CL/models
+cp Training/results/hebb/result/network/STL10_CIFAR100_CL/checkpoint.pth.tar Training/results/hebb/result/network/STL10_CIFAR100_CL/models/checkpoint.pth.tar
+conda run -n softhebb python continual_learning.py --preset 5SoftHebbCnnSTL --resume all --model-name 'STL10_CIFAR100_CL' --dataset-unsup-1 STL10_unlabel --dataset-sup-1 STL10_100aug --dataset-unsup-2 CIFAR100_1 --dataset-sup-2 CIFAR100_50 --continual_learning True --skip-1 True --evaluate True 
+
